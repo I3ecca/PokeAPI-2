3aca3e0b7dd79f762dc9220ee42ba511819b1ef4
RLant <rebeccalant@gmail.com>
2021-04-06 19:32:55 -0400

    Pkmn array sorted

--- scripts.js
+++ scripts.js
@@ -1,77 +1,75 @@
-$(function(){
+$(function() {
 
-    let pokeApiUrl="https://pokeapi.co/api/v2/generation/1";
-    let pokemonByNameUrl="https://pokeapi.co/api/v2/pokemon/";
+    let pokeApiUrl = "https://pokeapi.co/api/v2/generation/1";
+    let pokemonByNameUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-    
+
 
     //first call to the API gets all Gen1 data. 
-    $.getJSON(pokeApiUrl).done(function(data){
+    $.getJSON(pokeApiUrl).done(function(data) {
 
         //define empty array for pkmn info
         pkmnArray = [];
-        //Array prints out fine in order. 
-        console.log(pkmnArray);
-        // Error happens when I try to access an index. Comes up as Undefined :(
-        console.log(pkmnArray[0]);
 
         // Using the Gen1 Data, we loop though each pkmn species
-        $.each(data.pokemon_species, function(index, pokemon){
-            
-          //gives you the name with a capital first letter. 
-          let name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-          // create an anchor tag with necessary attributes and styling. 
-        //   let link = $("<a>").attr("id", pokemon.name).attr("href", "#").append("<strong>").text(name);
-          
-          // second call to API uses info from the first call to get more info on each pokemon. 
-          $.getJSON(pokemonByNameUrl + pokemon.name).done(function(info) {
+        $.each(data.pokemon_species, function(index, pokemon) {
 
-            pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+            pkmnArray.push(new Promise((resolve, reject) => {
+                //gives you the name with a capital first letter. 
+                let name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+                // create an anchor tag with necessary attributes and styling. 
+                //   let link = $("<a>").attr("id", pokemon.name).attr("href", "#").append("<strong>").text(name);
 
-            // create key value pairs
-            pkmn = {
-                name: pokemon.name,
-                id: info.id,
-                sprite_front: info.sprites.front_default,
-                sprite_back: info.sprites.back_default
-            };
+                // second call to API uses info from the first call to get more info on each pokemon. 
+                $.getJSON(pokemonByNameUrl + pokemon.name).done(function(info) {
 
-            // push the pkmn to the pkmnArray
-            pkmnArray.push(pkmn);
-            // Sort the pkmn Array
-            pkmnArray.sort((a, b) => {
-                return a.id - b.id;
-                });
-           
-            
+                    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
-            // specify what a paragraph will be
-            let par = $("<p>").html(`#${pkmn.id} is ${pkmn.name}`);
-            let sprite = `<img src = ${pkmn.sprite_front}>`;
+                    // create key value pairs
+                    pkmn = {
+                        name: pokemon.name,
+                        id: info.id,
+                        sprite_front: info.sprites.front_default,
+                        sprite_back: info.sprites.back_default
+                    };
 
-            let pokemonDiv = $("<div>").addClass("pokemon-containers").attr("id", `${pkmn.id}`).append(sprite).append(par);
+                    resolve(pkmn);
 
-            // Attach the paragraph to the page at the pokedex div. 
-            pokemonDiv.appendTo("#pokedex");
+                })
+            }));
 
-        
-          });
-          
-         
-         
 
-            
-        
-        
+
         });
-      }).fail(function(){
+
+        Promise.all(pkmnArray).then((sortedArray) => {
+
+            sortedArray.sort((a, b) => {
+                return a.id - b.id;
+            });
+
+            console.log(sortedArray);
+
+            $.each(sortedArray, function(index, value) {
+
+                let name = value.name.charAt(0).toUpperCase() + value.name.slice(1);
+
+                let para = $("<p>").html(`#${(value.id)} is ${name}`);
+
+                let sprite = `<img src = ${value.sprite_front}>`;
+
+                let pokemonDiv = $("<div>").addClass("pokemon-containers").attr("id", `${value.id}`).append(sprite).append(para);
+
+                // Attach the paragraph to the page at the pokedex div. 
+                pokemonDiv.appendTo("#pokedex");
+
+            })
+
+        })
+
+    }).fail(function() {
         console.log("Request to PokeAPI failed");
-      }).always(function(){
-        console.log("Request to PokeAPI successful")
-      })
-
-      
-      
-      
-
-  });
+    }).always(function() {
+        console.log("Pokemon is the best!")
+    })
+});
